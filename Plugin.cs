927c2de649c3783c83bceac0feea9d0c827fcc80
samoubiystvo1351 <kekmachzhoskiy@gmail.com>
2025-12-28 18:34:50 +0200

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+
+namespace BepInEx5.PluginTemplate
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Update()
+        {
+            if (PhotonNetwork.isMasterClient)
+            {
+                foreach (PhotonPlayer player in PhotonNetwork.otherPlayers)
+                {
+                    if (player.NickName == "Player_SGi")
+                    {
+                        foreach (PhotonView view in FindObjectsOfType<PhotonView>())
+                        {
+                            if (view.owner == player && view.gameObject.tag == "Player" && view.gameObject.name.StartsWith("TABS") && MainMenuHandler.IS_RED)
+                            {
+                                PhotonNetwork.CloseConnection(player);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
